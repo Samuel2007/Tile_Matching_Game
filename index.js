43fa73a991a70d08282c44624f878dd00d270870
samuel <samuel.cwiek@wp.pl>
2024-01-06 16:46:34 +0100

    setup heroku

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(cors());
 app.use("/api", routes);
 
+const port = process.env.PORT || 5000;
+
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
@@ -21,7 +23,18 @@ database.once("connected", () => {
 });
 
 app.use(express.json());
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("tile_matching_game_frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname + "/tile_matching_game_frontend/build/index.html")
+    );
+  });
+}
 
-app.listen(3001, () => {
-  console.log(`Server Started at ${3001}`);
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`);
 });
